6088a102e031ba55b3f78bc186e9ac47c9a31c4f
thegoodgamer14 <aryan.sgrbk@gmail.com>
2022-12-16 18:54:38 +0530

    Added Donor functionality

--- Blood_Bank.h
+++ Blood_Bank.h
@@ -41,6 +41,107 @@ void BloodBank::showDetails(int j, string bt) {
 void BloodBank::bloodBank(string s) {
     static int i = 0, j = 0;
 
+    if (s == "Add Donor") {
+        string n,m;
+        dc[i].setDonorID();
+
+        
+        cout<<"Enter name: ";
+        getline(cin>>n,m);
+        dc[i].setOwnerName(n+m);
+
+
+        cout<<"Enter donation date: ";
+        cin>>n;
+        dc[i].setDonationDate(n);
+
+        cout<<"Enter Aadhar number: ";
+        cin>>n;
+        dc[i].setAadhar(n);
+
+        cout<<"Enter Blood Group: ";
+        cin>>n;
+        dc[i].setBloodGroup(n);
+        
+        cout<<"Enter Phone number: ";
+        cin>>n;
+        dc[i].setPhone(n);
+
+        cout<<"Enter Email-ID: ";
+        cin>>n;
+        dc[i].setEmail(n);
+
+        i++;
+        cout<<"Donor details added successfully!";
+        
+        system("cls");
+    }
+
+    else if (s == "Display Donor List") {
+        system("cls");
+        int j;
+        for (j=0; j<i; j++){
+            showDetails(j);
+            cout<<"-----------------------------"<<endl;
+        }
+        system("pause");
+        system("cls");
+        if(j<1)
+            cout<<"Directory is empty!";
+    }
+
+    else if (s == "Search Donors List") {
+        string sea;
+        system("cls");
+        cout<<"Enter blood group to search for Donor"<<endl;
+        cin>>sea;
+        for (int j=0; j<i; j++) {
+            if (sea==dc[j].getBloodGroup()) {
+                showDetails(j);
+                system("pause");
+                system("cls");
+            }
+        }
+    }
+
+    else if (s=="Update Donor Details") {
+        string upd;
+        int ch;
+        system("cls");
+        cout<<"Enter Donor ID to update details"<<endl;
+        cin>>upd;
+        for (int j=0; j<i; j++)
+        {
+            if (upd==dc[j].getDonorID()) {
+                cout<<"Which detail do you want to update?"<<endl;
+                cout<<"1-Phone number"<<endl;
+                cout<<"2-Email ID"<<endl;
+                cout<<"Enter choice: ";
+                cin>>ch;
+                if (ch==1)
+                {
+                    string newPhn;
+                    cout<<"Enter new phone number: ";
+                    cin>>newPhn;
+                    dc[j].setPhone(newPhn);
+                    cout<<"New Phone number: "<<dc[j].getPhone()<<endl;
+                    cout<<"Donor details updated successfully!"<<endl;
+                }
+                else if (ch==2)
+                {
+                    string newEmail;
+                    cout<<"Enter new email: ";
+                    cin>>newEmail;
+                    dc[j].setEmail(newEmail);
+                    cout<<"New Email-ID: "<<dc[j].getEmail()<<endl;
+                    cout<<"Donor details updated successfully!"<<endl;
+                }
+            }
+        }
+        system("pause");
+        system("cls");
+    }
+
     if (s=="Add Recipient") {
         string n, m;
         rec[j].setRecipientID();
